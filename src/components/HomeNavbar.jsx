bcc003b16f1ae8d9eebaa2daab0d6f0f0f15db1e
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-10-15 10:30:54 -0700

    Slider adjust text center and background

--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -2,8 +2,8 @@
 function HomeNavbar({ cellCount, setCellCount }) {
   return (
     <nav className="sticky top-0 py-3 bg-slate-800 flex justify-center z-10">
-      <label htmlFor="steps-range" className="text-base font-medium text-gray-300">
-        Reel Symbols {cellCount} &nbsp;
+      <label htmlFor="steps-range" className="text-base font-medium text-center text-gray-300">
+        Reel Symbols {cellCount}
         <input
           id="steps-range"
           type="range"
@@ -12,7 +12,7 @@ function HomeNavbar({ cellCount, setCellCount }) {
           value={cellCount}
           step="1"
           onChange={(e) => setCellCount(e.target.value)}
-          className="w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          className="w-full h-3 rounded-lg appearance-none cursor-pointer bg-gray-700"
         />
       </label>
     </nav>
