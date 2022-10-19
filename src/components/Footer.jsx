7fb94b91395ff259ac0696af42aba2db36265737
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2022-10-19 10:18:50 -0700

    adjusted position of spin buttons based on screen size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types'
 
 function Footer({ handleRng, handleRngReverse }) {
   return (
-    <footer className="bg-gray-200 text-center fixed left-0 right-0 bottom-0 flex justify-center">
+    <footer className="bg-gray-200 text-center fixed left-0 right-0 bottom-0 flex justify-between lg:justify-around">
       <button
         type="button"
         onClick={handleRngReverse}
         className="bg-slate-800 hover:bg-slate-700 text-white font-bold py-2 px-4 border border-slate-900">
         SPIN
       </button>
-      <div className="text-gray-700 text-center p-3 font-normal text-base mx-auto">
+      <div className="text-gray-700 text-center p-3 font-normal text-base">
         © 2022 Michael Saucedo
       </div>
       <button
